c3df2f6107faecc9f3e82ad48b7de74f1329e9ce
axterrr <vgibskiy@gmail.com>
2024-11-10 19:23:09 +0200

    added thiserror

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
 use pest_derive::Parser;
+use thiserror::Error;
+use pest::error::Error as PestError;
 
 #[derive(Parser)]
 #[grammar = "./json_grammar.pest"]
 pub struct JsonGrammar;
+
+#[derive(Error, Debug)]
+pub enum JsonParserError {
+    #[error("Syntax error while parsing JSON: \n{0}")]
+    Syntax(#[from] PestError<Rule>),
+
+    #[error("Failed to read file: \n{0}")]
+    FileReadError(#[from] std::io::Error),
+}
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,36 @@
 mod cli;
-use std::{fs::read_to_string, path::PathBuf};
 use cli::{Action::*, CommandLineArgs};
-use structopt::StructOpt;
 use json_parser::*;
 use pest::Parser;
+use std::{fs::read_to_string, path::PathBuf};
+use structopt::StructOpt;
 
 fn main() {
     let CommandLineArgs { action } = CommandLineArgs::from_args();
 
     match action {
-        Parse { file } => {
-            match parse_json(&file) {
-                Ok(res) => {
-                    println!("File parsed successfully.");
-                    println!("{}", res);
-                },
-                Err(e) => eprintln!("Error parsing file: \n{}", e),
+        Parse { file } => match parse_json(&file) {
+            Ok(res) => {
+                println!("File parsed successfully.");
+                println!("{}", res);
             }
+            Err(e) => eprintln!("{}", e),
         },
         Help => {
             println!("Usage:");
             println!("  parse --file <PATH>  Parses the specified JSON file");
             println!("  help                 Displays this help message");
             println!("  credits              Displays credits information");
-        },
+        }
         Credits => {
             println!("JSON Parser CLI v1.0");
             println!("Created by Hibskyi Vladyslav");
-        },
+        }
     };
 }
 
-fn parse_json(file: &PathBuf) -> Result<String, String> {
-    let unparsed_file = read_to_string(file)
-        .map_err(|_| format!("Could not read file: {:?}", file))?;
-    
-    let res = JsonGrammar::parse(Rule::json, &unparsed_file)
-        .map_err(|e| format!("Parsing error: \n{}", e))?;
-    
+fn parse_json(file: &PathBuf) -> Result<String, JsonParserError> {
+    let unparsed_file = read_to_string(file)?;
+    let res = JsonGrammar::parse(Rule::json, &unparsed_file)?;
     Ok(format!("{}", res))
 }
